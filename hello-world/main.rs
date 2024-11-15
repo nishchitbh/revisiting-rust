48c6347d72065e8dbea33af2d0b4da9ef9217406
Nishchit Bhandari <nishchitbhandari2061@gmail.com>
2024-11-15 11:50:15 +0545

    Hello, world!: add reasoning comments

--- hello-world/main.rs
+++ hello-world/main.rs
@@ -1,3 +1,8 @@
-fn main(){
+fn main() {
     println!("Hello, world!");
 }
+// Few things:
+// 0. main() is the entry point of every Rust program
+// 1. println() is a function, println!() is a macro, will discuss more about macro in chapter 19
+// 2. println!() is used to print a line to the terminal, it takes a format string as an argument
+// 3. ; indicates the end of an expression, every expression in Rust ends with a semicolon
